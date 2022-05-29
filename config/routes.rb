d686d1b0c608a92773d36b7d67014605314ea222
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2022-05-29 22:31:39 +0545

    chore: remove unwanted changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #resource :users, only: [:create]
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
-
+  
   namespace :v1 do 
-
     resources :projects, only: [:create, :show, :update, :destroy] do
       resources :contents, only: [:index, :create, :show, :update, :destroy]
       collection do
@@ -14,7 +9,6 @@ Rails.application.routes.draw do
     end
 
     match '/projects', to: 'projects#index', via: [:get, :post]
-
     post "/users/signup", to: "users#create"
     post "/auth/signin", to: "users#login"
   end
